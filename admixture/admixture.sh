b1e579f941b6e606e88263fe6af25dec13d060b5
Maryam Ghoojaei <maryam.ghoojaei@gmail.com>
2026-08-24 17:09:06 -0700

    Update ADMIXTURE workflow for SLURM array jobs

--- admixture/admixture.sh
+++ admixture/admixture.sh
@@ -6,27 +6,43 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=4000
 #SBATCH -t 48:00:00
+#SBATCH --array=1-10
 
 cd /path/to/output_directory
 
-# Convert VCF to PLINK BED format
-/path/to/plink \
-  --vcf /path/to/snps_afterLD.vcf \
-  --allow-extra-chr \
-  --make-bed \
-  --out adinput
+K=$SLURM_ARRAY_TASK_ID
 
+# Only task 1 converts VCF to PLINK format
+if [ "$K" -eq 1 ]; then
 
-# Run ADMIXTURE for multiple K values
-for K in {1..10}; do
-  /path/to/admixture \
-    -B \
-    --cv=10 \
-    -j10 \
-    adinput.bed $K | tee log${K}.out &
+	/path/to/plink \
+  		--vcf /path/to/snps_afterLD.vcf \
+  		--allow-extra-chr \
+  		--make-bed \
+  		--out adinput
+  		
+  	# Save original BIM
+    cp adinput.bim adinput_original.bim
 
-  # Run up to 4 ADMIXTURE jobs simultaneously
-  [[ $((K % 4)) -eq 0 ]] && wait
+    # Replace chromosome column with integers
+    awk '{print NR, $2, $3, $4, $5, $6}' \
+      adinput.bim > adinput_rename.bim
+
+    # Use renamed BIM for ADMIXTURE
+    mv adinput_rename.bim adinput.bim
+
+    # Signal that preparation is completely finished
+    touch adinput.ready
+fi
+
+# Other array tasks wait until preparation is finished
+while [ ! -f adinput.ready ]; do
+    sleep 10
 done
 
-wait
+# Run ADMIXTURE 
+/path/to/admixture \
+-B \
+--cv=10 \
+-j10 \
+adinput.bed $K | tee log${K}.out 
